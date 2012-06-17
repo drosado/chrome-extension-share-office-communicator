3fb9018a5dc4c2a56f2d0302cac344e865a62e24
Daniel Rosado <daniel.rosado@gmail.com>
2012-06-17 23:27:49 +0100

    Initial draft of the extension. 'Send via IM' button does not work.
    Initial draft of the chrome extension.
    'Send via IM' button doesn ot work yet.

---
+++ popup.js
@@ -0,0 +1,29 @@
+
+function sendViaEmail() {
+    chrome.tabs.getSelected(null, function(tab) {
+        // tab object has all the information
+        window.open("mailto:?subject="+tab.title+"&body="+tab.url)
+    });
+}
+
+function sendViaIM() {
+    chrome.tabs.getSelected(null, function(tab) {
+        // tab object has all the information
+        copyToClipboard(tab.title+'\n'+tab.url)
+        window.open('sip:Some.Guy@there.com')
+        //window.open('im:rcrosado@gmail.com')
+        //window.open('gtalk:chat?jid=ml.rosado@gmail.com')
+    });
+}
+
+// Copy text to the clipboard function
+// uses 'url' input element to select text
+// parameters: text - text to be copied to the clipboard
+function copyToClipboard( text )
+{
+    var input = document.getElementById( 'url' );
+    input.value = text;
+    input.focus();
+    input.select();
+    document.execCommand( 'Copy' );
+}
